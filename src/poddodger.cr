090bf91fbdbd64ca4f734895f2d8e62b3e372d08
teesloane <tylersloane@gmail.com>
2020-06-06 15:44:21 -0400

    Add: enable setting output directory

--- src/poddodger.cr
+++ src/poddodger.cr
@@ -7,12 +7,12 @@ class Pododger
   property :feed_url, :out_dir
 
   @feed_url = String
-  @out_dir = String
+  @out_dir = String # should this be <String?> or <String> if user doesn't pass in -d to change out_dir ... I don't know.
   @mp3s = [] of Hash(String, String)
 
-  def intialize
-    @out_dir = ""  # should mark this as possibly nil: string?
-    @feed_url = "" # ditto
+  def initialize
+    @out_dir = nil # shouldn't this make
+    @feed_url = "" # should be nilable?
   end
 
   def download
@@ -20,12 +20,23 @@ class Pododger
     @mp3s.each { |mp3| save_file(mp3) } unless @mp3s.nil?
   end
 
+  def out_dir=(f : String)
+    @out_dir = f
+  end
+
   def save_file(mp3)
     spinner = Term::Spinner.new("[:spinner] Fetching: #{mp3["title"][(0..64)]} ...", format: :dots)
     spinner.auto_spin
     cleaned_title = mp3["title"].gsub("/", "-") # replace problem chars.
     resp = Halite.follow.get("#{mp3["url"]}")
-    File.write("#{cleaned_title}.mp3", resp.body)
+
+    if @out_dir
+      file_path = "#{@out_dir}/#{cleaned_title}.mp3"
+    else
+      file_path = "#{cleaned_title}.mp3"
+    end
+
+    File.write(file_path, resp.body)
     spinner.success("Done!")
   end
 
@@ -59,12 +70,19 @@ OptionParser.parse() do |parser|
   parser.separator
 
   parser.on("-f", "--feed string", "URL of podcast rss feed") { |u| pod.feed_url = u }
-  parser.on("-d", "--dir string", "Directory to save files to.") { |d| pod.out_dir = d }
-  parser.on("-h", "--help", "Show this help") { puts parser; exit }
 
-  parser.missing_option do |x|
-    puts "x #{x}"
+  parser.on("-d", "--dir string", "Directory to save files to.") do |dir|
+    if Dir.exists?(dir) == false
+      puts "Directory: #{dir} does not exist. Exiting."
+      exit
+    end
+    pod.out_dir = dir
   end
+
+  parser.on("-h", "--help", "Show this help") { puts parser; exit }
+  parser.separator
+
+  # Parser Error Handling
 
   parser.invalid_option do |flag|
     STDERR.puts "ERROR: #{flag} is not a valid option."
@@ -72,7 +90,10 @@ OptionParser.parse() do |parser|
     exit(1)
   end
 
-  parser.separator
+  if ARGV.empty?
+    puts parser
+    exit
+  end
 
   # if the first ARGV isn't one of the valid commans, throw an error.
   if ARGV.empty? || !["-f", "-h", "-d", "--feed", "--help", "--dir"].includes? ARGV.first
